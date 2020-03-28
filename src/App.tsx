095f81361d6b3f99c8a638161004cd07c2e243f9
Mario Fernandez <mario.fernandez@hceris.com>
2020-03-28 18:19:36 +0100

    base screen

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,18 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+import { CssBaseline, Container, Box, Typography } from "@material-ui/core";
+
+const App = () => (
+  <div data-testid="app">
+    <CssBaseline />
+
+    <Box mt={12}>
+      <Container component="main">
+        <Typography variant="h2">Can I ... ?</Typography>
+      </Container>
+    </Box>
+  </div>
+);
 
 export default App;

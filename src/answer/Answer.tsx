ff4d8fe974eaf5de83463bc5bf6f1151c0057b07
Mario Fernandez <mario.fernandez@hceris.com>
2020-03-28 23:43:23 +0100

    enhance footer

--- src/answer/Answer.tsx
+++ src/answer/Answer.tsx
@@ -1,13 +1,30 @@
 import React from 'react'
-import { Typography } from '@material-ui/core'
+
+import { Button, Typography } from '@material-ui/core'
+import { useHistory } from 'react-router-dom'
+
+import footer from './Footer.module.css'
 
 const Answer = () => {
+  const history = useHistory()
   return (
-    <section>
-      <Typography variant="h2" className="center" align="center">
-        No
-      </Typography>
-    </section>
+    <>
+      <section>
+        <Typography variant="h2" className="center" align="center">
+          No
+        </Typography>
+      </section>
+      <footer className={footer.footer}>
+        <Button
+          variant="contained"
+          color="primary"
+          onClick={() => history.push('/')}
+        >
+          Try your luck again
+        </Button>
+      </footer>
+    </>
   )
 }
+
 export default Answer

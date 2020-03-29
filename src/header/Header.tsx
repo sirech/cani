a1310972a50463d01e3dad52a5e0b1d726261145
Mario Fernandez <mario.fernandez@hceris.com>
2020-03-29 12:51:32 +0200

    make header clickable

--- src/header/Header.tsx
+++ src/header/Header.tsx
@@ -1,11 +1,24 @@
 import React from 'react'
 
 import { Typography } from '@material-ui/core'
+import { useHistory } from 'react-router-dom'
 
-const Header = () => (
-  <Typography variant="h2" align="center">
-    Can I ... ?
-  </Typography>
-)
+import styles from './Header.module.css'
+
+const Header = () => {
+  const history = useHistory()
+  return (
+    <header>
+      <Typography
+        className={styles.headline}
+        onClick={() => history.push('/')}
+        variant="h2"
+        align="center"
+      >
+        Can I ... ?
+      </Typography>
+    </header>
+  )
+}
 
 export default Header

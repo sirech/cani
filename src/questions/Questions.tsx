2aee9a91b1676331ac2c7e470bf2817dd10746ae
Mario Fernandez <mario.fernandez@hceris.com>
2020-03-29 12:26:10 +0200

    add more questions

--- src/questions/Questions.tsx
+++ src/questions/Questions.tsx
@@ -1,19 +1,50 @@
 import React from 'react'
-import { List, ListItem, Paper, Box, Fade } from '@material-ui/core'
+import {
+  List,
+  ListItem,
+  ListItemIcon,
+  Paper,
+  Box,
+  Fade,
+  ListItemText,
+} from '@material-ui/core'
 
 import { History } from 'history'
 import { useHistory } from 'react-router-dom'
 
+import DirectionsSubwayIcon from '@material-ui/icons/DirectionsSubway'
+import AirplanemodeActiveIcon from '@material-ui/icons/AirplanemodeActive'
+import BusinessCenterIcon from '@material-ui/icons/BusinessCenter'
+import OutdoorGrillIcon from '@material-ui/icons/OutdoorGrill'
+import LocalBarIcon from '@material-ui/icons/LocalBar'
+import MuseumIcon from '@material-ui/icons/Museum'
+import SportsHandballIcon from '@material-ui/icons/SportsHandball'
+
 import CustomIcon from '../customIcon'
 
-const questions = [
+type Image = string | JSX.Element
+
+const questions: Array<[string, Image]> = [
   ['walk my pangolin', 'pangolin.svg'],
   ['acquire toilet paper', 'toilet-paper.png'],
-  [' go to the bar'],
-  ['come from the bar'],
+  [' go to the bar', <LocalBarIcon />],
+  ['come from the bar', <SportsHandballIcon />],
+  ['grill outside', <OutdoorGrillIcon />],
+  ['take the subway', <DirectionsSubwayIcon />],
+  ['catch a plane', <AirplanemodeActiveIcon />],
+  ['go to work', <BusinessCenterIcon />],
+  ['go to the museum', <MuseumIcon />],
 ]
 
 const answerQuestion = (history: History) => history.push('/answer')
+
+const renderImage = (image: Image) => {
+  if (!image) return ''
+
+  const renderedImage =
+    typeof image === 'string' ? <CustomIcon source={image} /> : image
+  return <ListItemIcon>{renderedImage}</ListItemIcon>
+}
 
 export default () => {
   const history = useHistory()
@@ -24,8 +55,8 @@ export default () => {
           <List>
             {questions.map(([q, image]) => (
               <ListItem button key={q} onClick={() => answerQuestion(history)}>
-                {image && <CustomIcon source={image} />}
-                <span>{q}</span>
+                {renderImage(image)}
+                <ListItemText primary={q} />
               </ListItem>
             ))}
           </List>
